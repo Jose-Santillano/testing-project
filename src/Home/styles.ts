459cab5ab0a2fccc037fd33881e991499bd60786
José Santillano <jose.santillano7479@gmail.com>
2023-09-23 05:25:32 -0600

    Se añaden todos los archivos v1.0.0

---
+++ src/Home/styles.ts
@@ -0,0 +1,17 @@
+import { StyleSheet } from 'react-native';
+
+export const styles = StyleSheet.create({
+    container: {
+      flexGrow: 1,
+    },
+    camera: {
+      flex: 1,
+    },
+    containerText: {
+      backgroundColor: 'rgba(0,0,0,0.5)',
+      width: 50,
+      height: 50,
+    }
+});
+
+  
